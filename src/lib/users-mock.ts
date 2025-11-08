ca0e6f62de98919e708457eceb9a92a243df1d4d
Galih Suryo Wibowo <remenpaijo1@gmail.com>
2025-11-08 10:28:46 +0000

    ganti photo profil ke https://images.pexels.com/photos/415829/pexels-pho

--- src/lib/users-mock.ts
+++ src/lib/users-mock.ts
@@ -1,7 +1,7 @@
 import type { User } from './definitions';
 
-export const usersMockData: Omit<User, 'id'>[] = [
-  { users: 'admin', nik: '11111', nama_teknisi: 'Admin Utama', email: 'admin@example.com', password: 'password', role: 'Admin' },
+export const usersMockData: Omit<User, 'id' | 'permissions'>[] = [
+  { users: 'admin', nik: '11111', nama_teknisi: 'Admin Utama', email: 'admin@example.com', password: 'password', role: 'Admin', photoURL: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?cs=srgb&dl=pexels-pixabay-415829.jpg&fm=jpg' },
   { users: 'budi_s', nik: '22222', nama_teknisi: 'Budi Santoso', email: 'budi@example.com', password: 'password', role: 'Teknisi' },
   { users: 'siti_a', nik: '33333', nama_teknisi: 'Siti Aminah', email: 'siti@example.com', password: 'password', role: 'Teknisi' },
   { users: 'eko_p', nik: '44444', nama_teknisi: 'Eko Prasetyo', email: 'eko@example.com', password: 'password', role: 'Teknisi' },
