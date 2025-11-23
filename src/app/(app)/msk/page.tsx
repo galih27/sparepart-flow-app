7ed6cec468fe1f82c95bb9e75ab08fee5b1445d1
Galih Suryo Wibowo <remenpaijo1@gmail.com>
2025-11-23 16:23:58 +0000

    pakai database kita saja, jangan pakai dummy lagi

--- src/app/(app)/msk/page.tsx
+++ src/app/(app)/msk/page.tsx
@@ -1,8 +1,5 @@
 import MskClient from '@/components/app/msk-client';
-import { mskData } from '@/lib/data';
 
 export default function MskPage() {
-  const data = mskData;
-
-  return <MskClient data={data} />;
+  return <MskClient />;
 }
