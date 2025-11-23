89ad8cca10e72ff527f82debcebcaa8f448c2f6f
Galih Suryo Wibowo <remenpaijo1@gmail.com>
2025-11-23 17:47:31 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -39,4 +37,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
